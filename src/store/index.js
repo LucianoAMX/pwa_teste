ff0a34127bb28b090ee5644c089ccab2aab1f954
luciano_amx@hotmail.com <luciano_amx@hotmail.com>
2020-10-25 00:49:31 -0300

    pwa vue

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    dialog: false
+  },
+  getters: {
+    get_dialog: state => {
+      return state.dialog;
+    }
+  },
+  mutations: {
+    SET_DIALOG(state, dialog) {
+      state.dialog = dialog;
+    }
+  },
+  actions: {
+    set_dialog({ commit }, dialog) {
+      commit("SET_DIALOG", dialog);
+    }
+  },
   modules: {}
 });

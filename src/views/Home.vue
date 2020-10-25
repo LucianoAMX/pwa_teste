ab58d29c82b529c377a9ff1a88483bea6a39ab60
luciano_amx@hotmail.com <luciano_amx@hotmail.com>
2020-10-25 11:43:06 -0300

    dd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="ben vindo ao PWA teste" />
+    <HelloWorld msg="ben vindo ao PWA" />
   </div>
 </template>
 

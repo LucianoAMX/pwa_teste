c1d0ac96bbc27a0b4d7f26eba799bf81df2ff36b
luciano_amx@hotmail.com <luciano_amx@hotmail.com>
2020-10-25 01:02:29 -0300

    kk

---
+++ dist/precache-manifest.b6b61ed973e01133ca11c06bf65dda16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6c29da4c5eef7c8be0",
+    "url": "/css/app.bd70d789.css"
+  },
+  {
+    "revision": "cc3cafc906d0fd7a623d",
+    "url": "/css/chunk-vendors.40726481.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f5ac94824dcdcd10b647b940839212c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6462e10741466d132e76",
+    "url": "/js/about.c70d3933.js"
+  },
+  {
+    "revision": "df6c29da4c5eef7c8be0",
+    "url": "/js/app.4d1ad53c.js"
+  },
+  {
+    "revision": "cc3cafc906d0fd7a623d",
+    "url": "/js/chunk-vendors.4707fae1.js"
+  },
+  {
+    "revision": "2cca30b2ab826a6485460276d4526c73",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
